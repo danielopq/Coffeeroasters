705385c3bc87472571dd77f40dc83a4462c5e8f1
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-01 03:26:10 +0100

    footer finished

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,11 @@ import './footer.css';
 const Footer = () => {
     return (
         <footer>
-            <div id="footerLinks"></div>
+            <div id="footerLinks">
+                <div>HOME</div>
+                <div>ABOUT US</div>
+                <div>CREATE YOUR PLAN</div>
+            </div>
             <div id="footerSocialMedia">
                 <div id="facebook" className='socialMediaIcon'></div>
                 <div id="twitter" className='socialMediaIcon'></div>
