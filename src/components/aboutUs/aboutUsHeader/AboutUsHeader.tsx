18bbb6e24f9005306c03a20cd3ae3ea25d872242
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-02 15:26:54 +0100

    AboutUsHeader.tsx - finished

--- src/components/aboutUs/aboutUsHeader/AboutUsHeader.tsx
+++ src/components/aboutUs/aboutUsHeader/AboutUsHeader.tsx
@@ -1,8 +1,18 @@
-import './aboutUs.css';
+import './aboutUsHeader.css';
 
-const AboutUsHeader = ()=>{
-    return(
-        <header id="aboutUsHeader"></header>
+/**
+ * Component that renders the header for the "About Us" section.
+ * @returns {JSX.Element} - Header for the "About Us" section.
+ */
+const AboutUsHeader = () => {
+    return (
+        <header id="aboutUsHeader">
+            <h2>About Us</h2>
+            <p className='mainText'>Coffeeroasters began its journey of exotic discovery in 1999, highlighting
+                stories of coffee from around the world. We have since been dedicated to
+                bring the perfect cup - from bean to brew - in every shipment.
+            </p>
+        </header>
     )
 }
 export default AboutUsHeader;
