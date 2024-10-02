12ffdee3ef5ba00f0c6e6efaa9c0d55da9ec136d
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-02 14:52:22 +0100

    AboutUs.tsx - created

--- src/CoffeeRoastersApp.tsx
+++ src/CoffeeRoastersApp.tsx
@@ -1,12 +1,14 @@
-import './coffeeRoastersApp.css'
-import NavBar from './components/shared/navBar/NavBar'
-import Home from './components/home/Home'
+import './coffeeRoastersApp.css';
+import NavBar from './components/shared/navBar/NavBar';
+import Home from './components/home/Home';
+import AboutUs from './components/aboutUs/AboutUs';
 
 function CoffeeRoastersApp() {
   return (
     <>
       <NavBar />
-      <Home />
+      {/* <Home /> */}
+      <AboutUs />
     </>
   )
 }
---
+++ src/components/aboutUs/AboutUs.tsx
@@ -0,0 +1,15 @@
+import './aboutUs.css';
+import Footer from '../shared/footer/Footer';
+
+const AboutUs = () =>{
+    return(
+        <main id="aboutUs">
+            <header></header>
+            <div id="aboutUsOurCommitment"></div>
+            <div id="aboutUsQuality"></div>
+            <div id="aboutUsHeadQuarters"></div>
+            <Footer/>
+        </main>
+    )
+}
+export default AboutUs;
