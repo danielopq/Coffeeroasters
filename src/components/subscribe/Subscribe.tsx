73395928eccc3da55d5aa0d9535bba579da32184
danielopq <danielopq@gmail.com>
2024-10-03 17:00:11 +0100

    SubscribeHeader.tsx - finished

--- src/components/subscribe/Subscribe.tsx
+++ src/components/subscribe/Subscribe.tsx
@@ -1,8 +1,10 @@
 import './subscribe.css';
 import Footer from '../shared/footer/Footer';
+import SubscribeHeader from './subscribeHeader/SubscribeHeader';
 const Subscribe:React.FC = ()=>{
     return(
         <main id="subscribe">
+            <SubscribeHeader/>
             <Footer/>
         </main>
     )
--- src/components/subscribe/subscribeHeader/SubscribeHeader.tsx
+++ src/components/subscribe/subscribeHeader/SubscribeHeader.tsx
@@ -1,8 +1,19 @@
 import './subscribeHeader.css';
 
-const SubscribeHeader = () =>{
-    return(
-        <header id="subscribeHeader"></header>
+/**
+ * Component that renders the header for the subscribe page.
+ * @returns {JSX.Element} - The header for the subscribe page.
+ */
+const SubscribeHeader:React.FC = () => {
+    return (
+        <header id="subscribeHeader">
+            <h1>Create a plan</h1>
+            <p className='mainText'>
+                Coffee the way you wanted it to be. For coffee delivered tomorrow,
+                or next week. For whatever brew method you use. For choice,
+                for convenience, for quality.
+            </p>
+        </header>
     )
 }
 export default SubscribeHeader;
