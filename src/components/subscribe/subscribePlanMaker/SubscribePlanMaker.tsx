51ea2bfb0199395154a81212cbfc46ede8a3cff3
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-05 11:09:48 +0100

    SubscribePlanMaker.tsx - general layout

--- src/components/subscribe/subscribePlanMaker/SubscribePlanMaker.tsx
+++ src/components/subscribe/subscribePlanMaker/SubscribePlanMaker.tsx
@@ -1,8 +1,58 @@
 import './subscribePlanMaker.css';
 
-const SubscribePlanMaker:React.FC = ()=>{
-    return(
-        <div id="subscribePlanMaker"></div>
+import MainButton from '../../shared/mainButton/MainButton';
+
+const SubscribePlanMaker: React.FC = () => {
+    return (
+        <div id="subscribePlanMaker">
+            <div id="planMakerContent">
+                <div id="planIndex"></div>
+                <div id="planOptions">
+                    <div className='option'>
+                        <div className='optionTitle'></div>
+                        <div className='optionChoices'>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                        </div>
+                    </div>
+                    <div className='option'>
+                        <div className='optionTitle'></div>
+                        <div className='optionChoices'>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                        </div>
+                    </div>
+                    <div className='option'>
+                        <div className='optionTitle'></div>
+                        <div className='optionChoices'>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                        </div>
+                    </div>
+                    <div className='option'>
+                        <div className='optionTitle'></div>
+                        <div className='optionChoices'>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                        </div>
+                    </div>
+                    <div className='option'>
+                        <div className='optionTitle'></div>
+                        <div className='optionChoices'>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                            <div className='choice'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="planSummary"></div>
+            <MainButton enable={false} value='Create my plan!'/>
+        </div>
     )
 }
- export default SubscribePlanMaker;
+export default SubscribePlanMaker;
