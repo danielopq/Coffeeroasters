6ec7e4ff11be7bd3e2a004ef92ee51368dcce8bc
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-11 15:01:44 +0100

    SubscribePlamaker.tsx - index running

--- src/components/subscribe/subscribePlanMaker/SubscribePlanMaker.tsx
+++ src/components/subscribe/subscribePlanMaker/SubscribePlanMaker.tsx
@@ -11,8 +11,8 @@ interface planResume{
 const SubscribePlanMaker: React.FC = () => {
     const {option01,option02,option03,option04,option05} = OptionsData;
 
-    const [plan,setPlan] = useState<planResume>({drinkStyle:'_____',coffeeType:'_____',weight:'_____',grindingType:'_____',delivery:'_____'});
-    const {drinkStyle,coffeeType,weight,grindingType,delivery} = plan;
+    const [plan,setPlan] = useState<planResume>({preferences:'_____',beanType:'_____',quantity:'_____',grindOption:'_____',deliveries:'_____'});
+    const {preferences,beanType,quantity,grindOption,deliveries} = plan;
 
     const getChoice = (optionID:string, selectedChoice:string)=>{
         setPlan({...plan,[optionID]:selectedChoice});
@@ -21,24 +21,45 @@ const SubscribePlanMaker: React.FC = () => {
     return (
         <div id="subscribePlanMaker">
             <div id="planMakerContent">
-                <div id="planIndex"></div>
+                <div id="planIndex">
+                    <div className={preferences === '_____' ? 'indexOption indexUnselected' : 'indexOption' }>
+                        <p className='indexOptionNumber' style={{color:'var(--darkCyan)'}}>01</p>
+                        <p>Preferences</p>
+                    </div>
+                    <div className={beanType === '_____' ? 'indexOption indexUnselected' : 'indexOption' }>
+                        <p className='indexOptionNumber'>02</p>
+                        <p>Bean Type</p>
+                    </div>
+                    <div className={quantity === '_____' ? 'indexOption indexUnselected' : 'indexOption' }>
+                        <p className='indexOptionNumber'>03</p>
+                        <p>Quantity</p>
+                    </div>
+                    <div className={grindOption === '_____' ? 'indexOption indexUnselected' : 'indexOption' }>
+                        <p className='indexOptionNumber'>04</p>
+                        <p>Grind Option</p>
+                    </div>
+                    <div className={deliveries === '_____' ? 'indexOption indexUnselected' : 'indexOption' } style={{border:'none'}}>
+                        <p className='indexOptionNumber'>05</p>
+                        <p>Deliveries</p>
+                    </div>   
+                </div>
                 <div id="planOptions">
-                    <PlanOption optionID={'drinkStyle'} deployed={true} option={option01} getChoice={getChoice}/>
-                    <PlanOption optionID={'coffeeType'} deployed={false} option={option02} getChoice={getChoice}/>
-                    <PlanOption optionID={'weight'} deployed={false} option={option03} getChoice={getChoice}/>
-                    <PlanOption optionID={'grindingType'} deployed={false} option={option04} getChoice={getChoice}/>
-                    <PlanOption optionID={'delivery'} deployed={false} option={option05} getChoice={getChoice}/>
+                    <PlanOption optionID={'preferences'} deployed={true} option={option01} getChoice={getChoice}/>
+                    <PlanOption optionID={'beanType'} deployed={false} option={option02} getChoice={getChoice}/>
+                    <PlanOption optionID={'quantity'} deployed={false} option={option03} getChoice={getChoice}/>
+                    <PlanOption optionID={'grindOption'} deployed={false} option={option04} getChoice={getChoice}/>
+                    <PlanOption optionID={'deliveries'} deployed={false} option={option05} getChoice={getChoice}/>
                 </div>
             </div>
             <div id="planSummary">
                 <div id="planSummary-content">
                     <p className='mainText'>ORDER SUMMARY</p>
                     <p id="planSummary-resumeText">
-                    “I drink my coffee {drinkStyle == 'Capsules' ? 'using' : 'as'} <span className='planChoice'>{drinkStyle}</span>, 
-                    with a <span className='planChoice'>{coffeeType}</span> type of bean.
-                    <span className='planChoice'> {weight}</span>
-                    {drinkStyle === 'Capsules' ? '' : (<> ground ala <span className='planChoice'>{grindingType}</span></>)}
-                    , sent to me <span className='planChoice'>{delivery}</span>.”
+                    “I drink my coffee {preferences == 'Capsules' ? 'using' : 'as'} <span className='planChoice'>{preferences}</span>, 
+                    with a <span className='planChoice'>{beanType}</span> type of bean.
+                    <span className='planChoice'> {quantity}</span>
+                    {preferences === 'Capsules' ? '' : (<> ground ala <span className='planChoice'>{grindOption}</span></>)}
+                    , sent to me <span className='planChoice'>{deliveries}</span>.”
                     </p>
                 </div>
             </div>
