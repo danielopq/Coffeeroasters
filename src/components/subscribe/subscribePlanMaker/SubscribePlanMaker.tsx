ef899a9bede1c8fd17aa364890ed939d56e608ce
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-11 13:02:34 +0100

    subscribePlanmaker.tsx - getChoice working

--- src/components/subscribe/subscribePlanMaker/SubscribePlanMaker.tsx
+++ src/components/subscribe/subscribePlanMaker/SubscribePlanMaker.tsx
@@ -2,13 +2,20 @@ import './subscribePlanMaker.css';
 import OptionsData from './optionsData/OptionsData';
 import PlanOption from './planOption/PlanOption';
 import MainButton from '../../shared/mainButton/MainButton';
+import { useState } from 'react';
+
+interface planResume{
+    [option:string]:string;
+}
 
 const SubscribePlanMaker: React.FC = () => {
     const {option01,option02,option03,option04,option05} = OptionsData;
 
+    const [plan,setPlan] = useState<planResume>({drinkStyle:'',coffeeType:'',weight:'',grindingType:'',delivery:''});
+    
     const getChoice = (optionID:string, selectedChoice:string)=>{
-        console.log('optionID:', optionID);
-        console.log('selectedChoice:', selectedChoice);
+        setPlan({...plan,[optionID]:selectedChoice});
+        console.table(plan);
     }
 
     return (
@@ -16,11 +23,19 @@ const SubscribePlanMaker: React.FC = () => {
             <div id="planMakerContent">
                 <div id="planIndex"></div>
                 <div id="planOptions">
-                    <PlanOption optionID={'option01'} deployed={true} option={option01} getChoice={getChoice}/>
-                    <PlanOption optionID={'option02'} deployed={false} option={option02} getChoice={getChoice}/>
-                    <PlanOption optionID={'option03'} deployed={false} option={option03} getChoice={getChoice}/>
-                    <PlanOption optionID={'option04'} deployed={false} option={option04} getChoice={getChoice}/>
-                    <PlanOption optionID={'option05'} deployed={false} option={option05} getChoice={getChoice}/>
+                    <PlanOption optionID={'drinkStyle'} deployed={true} option={option01} getChoice={getChoice}/>
+                    <PlanOption optionID={'coffeeType'} deployed={false} option={option02} getChoice={getChoice}/>
+                    <PlanOption optionID={'weight'} deployed={false} option={option03} getChoice={getChoice}/>
+                    <PlanOption optionID={'grindingType'} deployed={false} option={option04} getChoice={getChoice}/>
+                    <PlanOption optionID={'delivery'} deployed={false} option={option05} getChoice={getChoice}/>
+                </div>
+            </div>
+            <div id="planSummary">
+                <div id="planSummary-content">
+                    <p className='mainText'>ORDER SUMMARY</p>
+                    <p id="planSummary-resumeText">
+                    “I drink my coffee using _____, with a _____ type of bean. _____ , sent to me _____.”
+                    </p>
                 </div>
             </div>
             <MainButton enable={false} value='Create my plan!'/>
