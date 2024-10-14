c4345622b9817366ba47b78764ac92805463ab7d
danielopq <danielopq@gmail.com>
2024-10-14 16:27:02 +0100

    main.tsx - BrowserRouter added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import CoffeeRoastersApp from './CoffeeRoastersApp'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import CoffeeRoastersApp from './CoffeeRoastersApp';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CoffeeRoastersApp />
+    <BrowserRouter>
+      <CoffeeRoastersApp />
+    </BrowserRouter>
   </StrictMode>,
 )
