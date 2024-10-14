eef99a968169924245146546c9d56e82a34838cd
danielopq <danielopq@gmail.com>
2024-10-14 17:19:10 +0100

    Home.tsx - MainButton navigation running

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,12 +5,24 @@ import MainButton from '../shared/mainButton/MainButton';
 import HomeCollection from './homeColletion/HomeCollection';
 import HomeWhyChooseUs from './homeWhyChooseUs/HomeWhyChooseUs';
 import PlanSteps from '../shared/planSteps/PlanSteps';
+import { useNavigate } from 'react-router-dom';
 
 /**
  * Renders the home page of the site.
  * @returns {JSX.Element} The home page component.
  */
 const Home: React.FC = () => {
+
+    const navigate = useNavigate();
+
+    /**
+     * Navigates to the specified path.
+     * @param {string} route - The path to navigate to.
+     */
+    const handleClick = (route: string): void => {
+        navigate(route);
+    };
+
     return (
         <main id="home">
             <HomeHeader/>
@@ -18,7 +30,7 @@ const Home: React.FC = () => {
             <HomeWhyChooseUs/>
             <h4>How it works?</h4>
             <PlanSteps fontColor='light'/>
-            <MainButton value={'Create your plan'}/>
+            <MainButton value={'Create your plan'} handleClick={()=> handleClick('/subscribe')}/>
             <Footer />
         </main>
     )
