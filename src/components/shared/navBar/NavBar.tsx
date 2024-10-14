8b26dbc0190304de9550ad19a410da952c734b5d
danielopq <danielopq@gmail.com>
2024-10-14 16:49:21 +0100

    NavBar.tsx - navigation running

--- src/components/shared/navBar/NavBar.tsx
+++ src/components/shared/navBar/NavBar.tsx
@@ -1,12 +1,26 @@
+import { useNavigate } from 'react-router-dom';
 import './navBar.css';
-
+/**
+ * Renders the main navigation bar of the website.
+ * @returns {JSX.Element} - The main navigation bar component.
+ */
 const NavBar: React.FC = () => {
+    const navigate = useNavigate();
+
+    /**
+     * Navigates to the specified path.
+     * @param {string} route - The path to navigate to.
+     */
+    const handleClick = (route: string): void => {
+        navigate(route);
+    };
+
     return (
         <nav id="mainNavBar">
             <div id="mainNavBarLinks">
-                <div>HOME</div>
-                <div>ABOUT US</div>
-                <div>CREATE YOUR PLAN</div>
+                <button onClick={() => handleClick('/')}>HOME</button>
+                <button onClick={() => handleClick('/aboutus')}>ABOUT US</button>
+                <button onClick={() => handleClick('/subscribe')}>CREATE YOUR PLAN</button>
             </div>
         </nav>
     )
