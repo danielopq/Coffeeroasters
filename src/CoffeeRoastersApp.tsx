837ea193109d3c66b7d3f42f8281035e08a80d9c
danielopq <danielopq@gmail.com>
2024-10-14 16:31:08 +0100

    CoffeRoasterApp.tsx - routes added

--- src/CoffeeRoastersApp.tsx
+++ src/CoffeeRoastersApp.tsx
@@ -3,14 +3,17 @@ import NavBar from './components/shared/navBar/NavBar';
 import Home from './components/home/Home';
 import AboutUs from './components/aboutUs/AboutUs';
 import Subscribe from './components/subscribe/Subscribe';
+import { Route, Routes } from 'react-router-dom';
 
 function CoffeeRoastersApp() {
   return (
     <>
       <NavBar />
-      {/* <Home /> */}
-      {/* <AboutUs /> */}
-      <Subscribe/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/subscribe" element={<Subscribe />} />
+      </Routes>
     </>
   )
 }
