577d3cd6a8986961c90f37fea401cea87deb0d6f
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-15 12:25:25 +0100

    HomeWhyChooseUs.tsx - tablet version done, desktop version modified

--- src/components/home/homeWhyChooseUs/HomeWhyChooseUs.tsx
+++ src/components/home/homeWhyChooseUs/HomeWhyChooseUs.tsx
@@ -9,7 +9,7 @@ const HomeWhyChooseUs = () => {
         <>
             <div id="homeWhyChooseUs">
                 <h2>Why choose us?</h2>
-                <p className="mainText">A large part of our role is choosing which particular 
+                <p className="mainText">A large part of our role is choosing which particular
                     coffees will be featured in our range. This means working
                     closely with the best coffee growers to give you a more
                     impactful experience on every level.
@@ -17,19 +17,22 @@ const HomeWhyChooseUs = () => {
             </div>
             <div id="homeReasons">
                 <div className="reason">
-                    <div id="coffeeBean-icon" className="reasonPic"></div>
-                    <h4>Best quality</h4>
-                    <p className="mainText">Discover an endless variety of the world’s best artisan coffee from each of our roasters.</p>
+                    <div id="coffeeBean-icon" className="reasonContent">
+                        <h4>Best quality</h4>
+                        <p className="mainText">Discover an endless variety of the world’s best artisan coffee from each of our roasters.</p>
+                    </div>
                 </div>
                 <div className="reason">
-                    <div id="gift-icon" className="reasonPic"></div>
-                    <h4>Exclusive benefits</h4>
-                    <p className="mainText">Special offers and swag when you subscribe, including 30% off your first shipment.</p>
+                    <div id="gift-icon" className="reasonContent">
+                        <h4>Exclusive benefits</h4>
+                        <p className="mainText">Special offers and swag when you subscribe, including 30% off your first shipment.</p>
+                    </div>
                 </div>
                 <div className="reason">
-                    <div id="truck-icon" className="reasonPic"></div>
-                    <h4>Free Shipping</h4>
-                    <p className="mainText">We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.</p>
+                    <div id="truck-icon" className="reasonContent">
+                        <h4>Free Shipping</h4>
+                        <p className="mainText">We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.</p>
+                    </div>
                 </div>
             </div>
         </>
