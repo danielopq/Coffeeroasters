4e1054a96376d414bda0ac9c0adf5388f617869f
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-16 00:57:37 +0100

    ConfirmationDialog.tsx - comments added

--- src/components/subscribe/subscribePlanMaker/confirmationDialog/ConfirmationDialog.tsx
+++ src/components/subscribe/subscribePlanMaker/confirmationDialog/ConfirmationDialog.tsx
@@ -13,6 +13,24 @@ interface PlanSummaryProps {
     displayDialog:()=>void;
 }
 
+
+/**
+ * Renders a confirmation dialog that displays the summary of the selected plan options, 
+ * the total price, and a button for confirming the order.
+ * It provides users with a final review of their choices before proceeding to checkout.
+ * 
+ * @param {PlanSummaryProps} props - The properties for the component:
+ * @param {string} props.preferences - The selected coffee preferences.
+ * @param {string} props.beanType - The type of coffee bean chosen.
+ * @param {string} props.quantity - The quantity of coffee selected.
+ * @param {string} props.grindOption - The grind type selected (if applicable).
+ * @param {string} props.deliveries - The delivery frequency selected.
+ * @param {boolean} props.showDialog - Whether the dialog is currently visible.
+ * @param {number} props.finalPrice - The total price for the plan.
+ * @param {function} props.displayDialog - Function to toggle the visibility of the dialog.
+ * 
+ * @returns {JSX.Element} - The rendered confirmation dialog component.
+ */
 const ConfirmationDialog: React.FC<PlanSummaryProps> = ({ preferences, beanType, quantity, grindOption, deliveries,showDialog=false,finalPrice,displayDialog }) => {
 
     useEffect(()=>{
