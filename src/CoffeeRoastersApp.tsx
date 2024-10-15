0caf39f1abdb6ede9c1e4c305745f2931afd5808
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-15 14:01:54 +0100

    subscribeSteps.css - tablet version done

--- src/CoffeeRoastersApp.tsx
+++ src/CoffeeRoastersApp.tsx
@@ -1,4 +1,3 @@
-import './coffeeRoastersApp.css';
 import NavBar from './components/shared/navBar/NavBar';
 import Home from './components/home/Home';
 import AboutUs from './components/aboutUs/AboutUs';
