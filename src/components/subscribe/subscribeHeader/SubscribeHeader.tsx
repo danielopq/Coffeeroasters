65afd537b8ae82bcbce7f56634cc7127ebbd4c05
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-15 13:49:38 +0100

    subscribeHeader.css - tablet version done

--- src/components/subscribe/subscribeHeader/SubscribeHeader.tsx
+++ src/components/subscribe/subscribeHeader/SubscribeHeader.tsx
@@ -4,14 +4,14 @@ import './subscribeHeader.css';
  * Component that renders the header for the subscribe page.
  * @returns {JSX.Element} - The header for the subscribe page.
  */
-const SubscribeHeader:React.FC = () => {
+const SubscribeHeader: React.FC = () => {
     return (
         <header id="subscribeHeader">
             <h1>Create a plan</h1>
             <p className='mainText'>
-                Coffee the way you wanted it to be. For coffee delivered tomorrow,
-                or next week. For whatever brew method you use. For choice,
-                for convenience, for quality.
+                Build a subscription plan that best fits your needs.
+                We offer an assortment of the best artisan coffees from
+                around the globe delivered fresh to your door.
             </p>
         </header>
     )
