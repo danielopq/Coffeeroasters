a578cf817aa12341c5430ea8d63238393a54b28e
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-16 00:46:28 +0100

    PlanChoice.tsx - tablet version done, comments added

--- src/components/subscribe/subscribePlanMaker/planOption/planChoice/PlanChoice.tsx
+++ src/components/subscribe/subscribePlanMaker/planOption/planChoice/PlanChoice.tsx
@@ -11,6 +11,17 @@ interface ChoiceProps{
     onClick: () => void;
 }
 
+/**
+ * Renders a selectable choice for a product option within the plan.
+ * The component displays the choice's title and description, and highlights
+ * the selection status based on the `selected` prop.
+ *
+ * @param {ChoiceProps} props - The properties for the component:
+ * @param {Choice} props.choiceProperties - Contains the title and description of the choice.
+ * @param {boolean} props.selected - Indicates if the choice is currently selected.
+ * @param {function} props.onClick - Function to be called when the choice is clicked.
+ * @returns {JSX.Element} The rendered component for the product choice.
+ */
 const PlanChoice: React.FC<ChoiceProps> = ({choiceProperties,selected=false,onClick}) => {
     return (
         <div className={selected ? 'choice selectedChoice' : 'choice unSelectedChoice'} onClick={onClick}>
