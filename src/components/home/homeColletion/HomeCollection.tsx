d85bccdaf2cac5f5c8d957352b3e8e0711fb7b86
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-18 00:48:32 +0100

    HomeCollection.tsx - new grid structure, Mobile version done

--- src/components/home/homeColletion/HomeCollection.tsx
+++ src/components/home/homeColletion/HomeCollection.tsx
@@ -9,26 +9,30 @@ const HomeCollection: React.FC = () => {
         <div id="homeCollection">
             <div id="homeOurCollection">our collection</div>
             <div id="homeCoffeVarieties">
-                <div id="homeCoffeePictures">
+                <div id="granExpreso" className='variety'>
                     <div id="granExpresoPic" className='coffeePacketPic'></div>
-                    <div id="planaltoPic" className='coffeePacketPic'></div>
-                    <div id="picolloPic" className='coffeePacketPic'></div>
-                    <div id="danchePic" className='coffeePacketPic'></div>
-                </div>
-                <div id="homeCoffeFeatures">
-                    <div className='homeCoffeVariety'>
+                    <div id="granExpresoDesc" className='homeCoffeVariety'>
                         <div className='homeCoffeeName'>Gran Espresso</div>
                         <div className='homeDescription mainText'>Light and flavorful blend with cocoa and black pepper for an intense experience.</div>
                     </div>
-                    <div className='homeCoffeVariety'>
+                </div>
+                <div id="planalto" className='variety'>
+                    <div id="planaltoPic" className='coffeePacketPic'></div>
+                    <div id="planaltoDesc" className='homeCoffeVariety'>
                         <div className='homeCoffeeName'>Planalto</div>
                         <div className='homeDescription mainText'>Brazilian dark roast with rich and velvety body, and hints of fruits and nuts.</div>
                     </div>
-                    <div className='homeCoffeVariety'>
+                </div>
+                <div id="picollo" className='variety'>
+                    <div id="picolloPic" className='coffeePacketPic'></div>
+                    <div id="picolloDesc" className='homeCoffeVariety'>
                         <div className='homeCoffeeName'>Picollo</div>
                         <div className='homeDescription mainText'>Mild and smooth blend featuring notes of toasted almond and dried cherry.</div>
                     </div>
-                    <div className='homeCoffeVariety'>
+                </div>
+                <div id="danche" className='variety'>
+                    <div id="danchePic" className='coffeePacketPic'></div>
+                    <div id="dancheDesc" className='homeCoffeVariety'>
                         <div className='homeCoffeeName'>Danche</div>
                         <div className='homeDescription mainText'>Ethiopian hand-harvested blend densely packed with vibrant fruit notes.</div>
                     </div>
