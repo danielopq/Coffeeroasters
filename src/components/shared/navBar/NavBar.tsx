570d7242fff1886d61763ebd49b6d2d69a5e7db4
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-17 12:47:20 +0100

    NavBar.tsx - mobile menu version done

--- src/components/shared/navBar/NavBar.tsx
+++ src/components/shared/navBar/NavBar.tsx
@@ -1,28 +1,54 @@
 import { useNavigate } from 'react-router-dom';
 import './navBar.css';
+import { useEffect, useRef, useState } from 'react';
 /**
  * Renders the main navigation bar of the website.
  * @returns {JSX.Element} - The main navigation bar component.
  */
 const NavBar: React.FC = () => {
+
+    const refIcon = useRef<HTMLButtonElement>(null);
     const navigate = useNavigate();
+    const [mobileMenuVisible, setMobileMenuVisible] = useState<boolean>(false);
+
+    useEffect(() => {
+        mobileMenuVisible ? document.body.style.position = 'fixed' : document.body.style.position = '';
+        if (refIcon.current) {
+            refIcon.current.className === 'showBurgerIcon' ? refIcon.current.className = 'showCloseIcon' : refIcon.current.className = 'showBurgerIcon';
+        }
+
+    }, [mobileMenuVisible])
 
     /**
      * Navigates to the specified path.
      * @param {string} route - The path to navigate to.
      */
-    const handleClick = (route: string): void => {
+    const navigateTo = (route: string): void => {
         navigate(route);
+        setMobileMenuVisible(false);
     };
 
+    const displayMobileMenu = (): void => {
+        setMobileMenuVisible(!mobileMenuVisible);
+    }
+
     return (
-        <nav id="mainNavBar">
-            <div id="mainNavBarLinks">
-                <button onClick={() => handleClick('/')}>HOME</button>
-                <button onClick={() => handleClick('/aboutus')}>ABOUT US</button>
-                <button onClick={() => handleClick('/subscribe')}>CREATE YOUR PLAN</button>
-            </div>
-        </nav>
+        <>
+            <nav id="mainNavBar">
+                <div id="mainNavBarLinks">
+                    <button onClick={() => navigateTo('/')}>HOME</button>
+                    <button onClick={() => navigateTo('/aboutus')}>ABOUT US</button>
+                    <button onClick={() => navigateTo('/subscribe')}>CREATE YOUR PLAN</button>
+                </div>
+                <button ref={refIcon} id="menuMobileIcon" className='showBurgerIcon' onClick={displayMobileMenu}></button>
+            </nav>
+            <menu id="mobileMenu" className={mobileMenuVisible ? 'mobileMenuDisplayed' : 'mobileMenuHidden'}>
+                <button onClick={() => navigateTo('/')}>HOME</button>
+                <button onClick={() => navigateTo('/aboutus')}>ABOUT US</button>
+                <button onClick={() => navigateTo('/subscribe')}>CREATE YOUR PLAN</button>
+            </menu>
+        </>
+
     )
 }
 export default NavBar;
